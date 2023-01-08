42dd81aacafef6bc62bcb2500ec4ab422ca6dd63
Luiz Meneghim <luiz.meneghim@hotmail.com>
2023-01-08 11:05:17 -0700

    doing extra credit

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import useWindowSize from 'react-use/lib/useWindowSize';
 import Confetti from 'react-confetti';
 
 /**
+ * What else to do:
  * 1) Track number of rolls
  * 2) How long did it take to win?
  * 3) Save the best time in local storage
